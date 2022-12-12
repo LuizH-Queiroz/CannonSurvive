2079183de5befc63fd5243ca28c1e29661090659
Luiz Henrique Queiroz de Albuquerque Silva <83130948+LuizH-Queiroz@users.noreply.github.com>
2022-12-12 18:03:27 -0300

    Follower (enemy) in PlayState

--- src/states/game/PlayState.lua
+++ src/states/game/PlayState.lua
@@ -1,18 +1,58 @@
 PlayState = Class{__includes = BaseState}
 
 
+
 function PlayState:init()
-    self.player = Player()
+    gPlayer = Player()
+
+    self.enemies = {
+        Follower()
+    }
+    self.enemySpawnTime = math.random(5, 8)
 end
 
 
 
 function PlayState:update(dt)
-    self.player:update(dt)
+    -- player update
+    gPlayer:update(dt)
+
+
+    -- Enemies handling:
+    -- -> Spawn
+    -- -> Update
+    -- -> Removal (TODO)
+    self.enemySpawnTime = self.enemySpawnTime - dt
+    if self.enemySpawnTime <= 0 then
+        
+        InstantiateNewEnemy(self.enemies)
+        self.enemySpawnTime = math.random(1, 3)
+    end
+
+    for i, enemy in pairs(self.enemies) do
+
+        enemy:update(dt)
+    end
 end
 
 
 
 function PlayState:render()
-    self.player:render()
+    gPlayer:render()
+
+    for i, enemy in pairs(self.enemies) do
+    
+        enemy:render()
+    end
+end
+
+
+
+function InstantiateNewEnemy(enemiesTable)
+
+    local enemies = {
+        [1] = function() return Follower() end
+    }
+
+    table.insert(enemiesTable, enemies[math.random(1, #enemies)]())
 end
