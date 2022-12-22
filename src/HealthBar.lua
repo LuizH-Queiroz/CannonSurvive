31664b109bfa98b6c57ad4ae3b0a613119f0920c
Luiz Henrique Queiroz de Albuquerque Silva <83130948+LuizH-Queiroz@users.noreply.github.com>
2022-12-22 17:02:10 -0300

    HealthBar Correction + Centering

--- src/HealthBar.lua
+++ src/HealthBar.lua
@@ -22,7 +22,6 @@ function HealthBar:update(dt)
     if self.autoHeal_Timer == 0 then
         
         self:heal(self.autoHeal_Amount * dt)
-        self.autoHeal_Timer = self.autoHeal_Cooldown
     end
 end
 
@@ -31,7 +30,12 @@ function HealthBar:render(x, y, width, height)
 
     love.graphics.setColor(255/255, 6/255, 0/255, 1)
 
-    love.graphics.rectangle('fill', x, y, (self.currentHealth/self.maxHealth) * width, height)
+    love.graphics.rectangle('fill',
+        x + ((1 - self.currentHealth/self.maxHealth) * width)/2,
+        y,
+        (self.currentHealth/self.maxHealth) * width,
+        height
+    )
 end
 
 
@@ -42,14 +46,14 @@ end
 
 function HealthBar:damage(amount)
 
-    self.currenthealth = math.max(0, self.currenthealth - amount)
-    self.autoHealTimer = self.autoHeal_Cooldown
+    self.currentHealth = math.max(0, self.currentHealth - amount)
+    self.autoHeal_Timer = self.autoHeal_Cooldown
 end
 
 
 function HealthBar:heal(amount)
 
-    self.currenthealth = math.min(self.currenthealth + amount, self.maxHealth)
+    self.currentHealth = math.min(self.currentHealth + amount, self.maxHealth)
     if self.currentHealth == self.maxHealth then
         
         self.autoHeal_Timer = self.autoHeal_Cooldown
